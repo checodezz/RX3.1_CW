9a12527accd11617021c19bb716f599ae8a1d919
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-07-29 12:22:21 +0530

    added like button

--- src/features/posts/Posts.jsx
+++ src/features/posts/Posts.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
-
+import { useDispatch } from "react-redux";
+import { likeButtonPressed } from "./postSlice";
 const Posts = () => {
+  const dispatch = useDispatch();
   const posts = useSelector((state) => {
     console.log(state.posts);
     return state.posts;
@@ -10,7 +12,9 @@ const Posts = () => {
       {posts.posts.map((post) => (
         <div key={post.id}>
           <p>{post.caption}</p>
-          <p>Likes{post.likes}</p>
+          <button onClick={() => dispatch(likeButtonPressed(post.postId))}>
+            {post.likes} Likes
+          </button>
         </div>
       ))}
     </div>
